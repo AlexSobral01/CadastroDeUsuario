e2ede9089c711b19b931966f46dadb158fa3833f
AlexSobral01 <alex.dacostasobral@gmail.com>
2025-12-05 22:50:21 -0300

    first commit

---
+++ src/main/java/com/cadastroDeUsuario/CadastroDeUsuario.java
@@ -0,0 +1,12 @@
+package com.cadastroDeUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuario {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeUsuario.class, args);
+    }
+}
---
+++ src/test/java/com/cadastroDeUsuario/alek/AlekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastroDeUsuario.alek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
